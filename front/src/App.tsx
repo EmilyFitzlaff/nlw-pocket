95522b313f32bedffe245210867ffe91f3e77513
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2024-09-12 17:21:40 -0300

    Finalização da aula 03

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,20 @@
 import { Dialog } from "./components/ui/dialog";
-import { CreateGoal } from "./components/CreateGoal";
 import { Summary } from "./components/Summary";
+import { EmptyGoals } from "./components/EmptyGoals";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
+import { CreateGoal } from "./components/CreateGoal";
 
 export function App() {
+	const { data } = useQuery({
+		queryKey: ["summary"],
+		queryFn: getSummary,
+		staleTime: 1000 * 60, // 60 seconds
+	});
+
 	return (
 		<Dialog>
-			<Summary />
-			{/* <EmptyGoals /> */}
+			{data && data?.total > 0 ? <Summary /> : <EmptyGoals />}
 			<CreateGoal />
 		</Dialog>
 	);
